3f277b975e40388d0603862bc2335f664950333f
AZuoza <azuoza@gmail.com>
2014-05-25 20:38:56 +0200

    final version

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-GCD_Project1
+Getting and Cleaning Data Course Project 1
 ============
 
-Getting and Cleaning Data Course Project
+Date: 25.05.2013
+Author: A.Zuoza
+
+The goals of this scrip are:
+- to read and merge test and train data sets.
+- to rename variables names.
+- to form the final data set with only mean and standard deviation for each measurement.
+- to create second data set, with the average of each variable for each activity and each subject.
+
+Reading data
+------------
+The data was downloaded on 14.04.2014 from:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+This script assumes that the data was unziped in the Data directory wich is in the current working directory. It is expecting that the names and path of the data are as follows:
+
+|Data file     | Path                   |
+|:--------------:|:-----------------------:|
+|X_test data   |./Data/test/X_test.txt  |
+|y_test data   |./Data/test/y_test.txt  |
+|X_train data  |./Data/train/X_train.txt|
+|y_train data  |./Data/train/y_train.txt|
+|test subjects |./Data/test/subject_test.txt|
+|train subjects|./Data/train/subject_train.txt|
+|activity labels|./Data/activity_labels.txt|
+|variable names |./Data/features.txt|
+
+
+Renaming variable names
+-----------------------
+
+Variables renamed according to the Google R style guide.
+By renaming of variables the folowing changes was made:
+- minus signs (-) was replaced with dots (.).
+- brackets () was replaced with dots (.).
+- commas (,) was replaced with dots (.). 
+- doble dots (..) was replaced with one dot (.).
+- dots (.) at the end of variable name was removed.
+- the small and capital letters have not been chnaged.
+
+Final data set
+---------------
+
+Final data set was saved in the file named "final_ds.csv", in the current working directory. 
+
+Folowig properties was used:
+- Field separator: ;
+- Decimal separator: .
+
+Second data set
+---------------
+
+The second data set was saved in the file "aver_ds.csv", in the current working directory.
+
+Folowig properties was used:
+- Field separator: ;
+- Decimal separator: .
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,138 @@
-run_analysis.R
+
+# input: none
+# output: 
+#        1. formated data frame with all subjects
+#        2. averaged data frame for all subjects and all activities
+
+# author: A.Zuoza
+# email: azuoza(at)gmail(dot)com
+
+# Part 1
+# description: script reads Human Activity Recognition Using Smartphones Dataset
+# and returns formated data frame. 
+
+
+# reading data
+x.train <- read.table("./Data/train/X_train.txt")
+y.train <- read.table("./Data/train/y_train.txt")
+sub.train <- read.table("./Data/train/subject_train.txt")
+x.test <- read.table("./Data/test/X_test.txt")
+y.test <- read.table("./Data/test/y_test.txt")
+sub.test <- read.table("./Data/test/subject_test.txt")
+kColNames <- read.table("./Data/features.txt")
+act.labes <- read.table("./Data/activity_labels.txt")
+
+# adding column names
+kColNames <- as.character(kColNames[,-1])
+kColNames <- sapply(kColNames, 
+                    FUN=function(x){
+                        x <- gsub("-", ".", x)    # replacing - with .
+                        x <- gsub("\\(\\)", ".", x)    # replacing () with .
+                        x <- gsub("\\(", ".", x)    # replacing ( with .
+                        x <- gsub("\\)", ".", x)    # replacing ) with .
+                        x <- gsub("\\,", ".", x)    # replacing , with .
+                        x <- gsub("\\.\\.", ".", x)    # replacing .. with .
+                        x <- gsub("(\\.)$", "", x)    # removing . at the end
+                        }
+                    )
+kColNames <- as.character(kColNames)
+
+colnames(x.train) <- kColNames
+colnames(x.test) <- kColNames
+
+rm(kColNames)    # removving kColNames from memmory
+
+# adding activity labels to y
+# converting act.labes from data frame to char vector
+act.labes <- as.character(act.labes$V2)
+act.labes <- tolower(act.labes)
+act.labes <- sapply(act.labes, 
+                    FUN=function(x){
+                        x <- gsub("_",".",x)    # replacing _ with .
+                    })
+act.labes <- as.character(act.labes)
+
+# converting y from data frame to factor vectors
+y.train <- factor(
+        x=as.integer(y.train$V1),
+        labels=act.labes
+        )
+y.test <- factor(
+        x=as.integer(y.test$V1),
+        labels=act.labes
+        )
+
+# adding x and y together
+
+test <- x.test
+test$activity <- y.test
+test$subject <- sub.test$V1
+
+train <- x.train
+train$activity <- y.train
+train$subject <- sub.train$V1
+
+ds <- rbind (train, test)
+
+rm(x.test, x.train, y.test, y.train, test, train, sub.test, sub.train)
+gc()             # gabage colection
+
+# finding columns with mean and standart deviation
+
+col.index <- grepl(pattern="(.*[mM]ean.*)|(.[sS][tT][dD]*)|(activity)|(subject)",
+                   names(ds))
+ds <- ds[, col.index]
+
+rm(col.index)
+
+# saving data set to the file 
+
+write.table(x=ds, file="final_ds.cvs", dec=".", sep=";")
+
+# Part 2
+# Data set, with the average of each variable for each activity and each subject.
+
+#making subjects vector 
+sub.vec <- unique(ds$subject)
+aver.ds <- data.frame(matrix(ncol=dim(ds)[2]))
+colnames(aver.ds) <- names(ds)
+row.numbers <- 1  #row number index
+
+for(i in 1:length(sub.vec)){
+    # subseting only ones subjects data
+    subs.subject <- subset(ds, ds$subject==i)
+    # subseting activity and calculating average
+    for(j in 1:length(act.labes)){
+        
+        print(paste("eilutes nr ", row.numbers, "activity ", j, "subject ", i))
+        
+        # making activity subset
+        subs.activity <- subset(subs.subject, subs.subject$activity==act.labes[j])
+        rownames(subs.activity) <- NULL
+        if(dim(subs.activity)[1] != 0){
+            
+            #browser()
+            
+            #calcucating mean
+            aver.val <- sapply(subs.activity[, 1:86], mean)
+            #adding activity
+            aver.val <- as.numeric(aver.val)
+            aver.val[87] <- NA
+            #adding subject
+            aver.val[88] <- sub.vec[i]
+            # copy to aver.ds data set
+            aver.ds[row.numbers, ] <- aver.val
+            # follow row.numbers
+            # adding activity label
+            aver.ds$activity <- as.character(aver.ds$activity)
+            aver.ds$activity[row.numbers] <- act.labes[j]
+            row.numbers <- row.numbers + 1
+        }
+    }
+}
+# convert activity to factor
+aver.ds$activity <- as.factor(aver.ds$activity)
+# write data set to file 
+write.table(x=aver.ds, file="aver_ds.csv", dec=".", sep=";")
+
+rm(act.labes)    # removing act.labes from memmory
